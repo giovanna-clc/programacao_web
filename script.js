2d2c9e4d23e09ea7f22c86a493a991b179ccf555
giovanna-clc <giovanna.couto@sempreceub.com>
2024-08-13 10:37:25 -0300

    Novo tratamento de erro do CPF

--- script.js
+++ script.js
@@ -19,6 +19,18 @@ function validaCPF(cpf) {
         alert("Campo CPF não pode ser vazio");
         return false;
     }
-    cpf = cpf.trim();
+    cpf = cpf.trim();                                           //Para retirar os espaços que o usuário pode colocar
+
+    if(/[a-zA-Z]/.test(cpf)) {
+        //console.log("Contém letras"); //Aviso que contém letras para mim no console do chrome
+        alert ("CPF não pode conter letras");
+        return false;
+    }
+
+    if (!/^[\d.-]+$/.test(cpf)) {                               // \d é o equivalente a [0-9], ^início $fim de texto +uma ou mais vezes !se não for... (é um caracter de negação)
+        alert ("O CPF só pode conter números, '.' ou '-'");     //verificar se é composto somente de números. "." ou "-"
+        return false;
+    }
+
     return true;
 }
