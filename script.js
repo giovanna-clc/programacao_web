e10d4063d9f7854ec40c215c7c4d9ed66a53a1bb
giovanna-clc <giovanna.couto@sempreceub.com>
2024-08-13 10:13:27 -0300

    Tratamento de erro do cpf

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function submeter () {
     console.log (cpf);
     console.log(validaCPF(cpf));
 }
+
 function validaCPF(cpf) {
     if (cpf == "") {
         alert("Campo CPF não pode ser vazio");
